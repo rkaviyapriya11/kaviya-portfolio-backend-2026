688a232511537337d04dcafe5a36e20c4645670f
rkaviyapriya <rkaviyapriya11@gmail.com>
2026-07-13 11:17:55 +0530

    database changed to cloud

--- src/database.ts
+++ src/database.ts
@@ -1,21 +1,38 @@
 import { Sequelize } from 'sequelize';
 
 
+// const sequelize = new Sequelize({
+//   dialect: 'mysql',
+//   host: '127.0.0.1',
+//   port: 3306,
+//   username: 'kaviya_db_user',
+//   password: '123Kaviyadb!@#',
+//   database: "kaviya_portfolio", 
+//   logging: false,
+//   pool: {
+//     max: 100, // Increase this number
+//     min: 0,
+//     acquire: 120000, // Increase the timeout value (milliseconds)
+//     idle: 10000
+//   }
+// });
+
 const sequelize = new Sequelize({
   dialect: 'mysql',
-  host: '127.0.0.1',
-  port: 3306,
-  username: 'kaviya_db_user',
-  password: '123Kaviyadb!@#',
-  database: "kaviya_portfolio", 
+  host: process.env.host,
+  port: Number(process.env.port),
+  username: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
   logging: false,
   pool: {
-    max: 100, // Increase this number
+    max: 100,
     min: 0,
-    acquire: 120000, // Increase the timeout value (milliseconds)
-    idle: 10000
-  }
+    acquire: 120000,
+    idle: 10000,
+  },
 });
+
 
 
 sequelize.authenticate()
